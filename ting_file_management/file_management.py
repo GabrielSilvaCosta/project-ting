d042b8c9c19307159fb23ede5327e0f67d08df48
GabrielSilvaCosta <bob@>
2023-10-27 18:17:52 -0300

    feat: file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+
+    except (FileNotFoundError, ValueError) as e:
+        print(
+            f"Arquivo {path_file} não encontrado"
+            if isinstance(e, FileNotFoundError)
+            else str(e),
+            file=sys.stderr,
+        )
+        return None
