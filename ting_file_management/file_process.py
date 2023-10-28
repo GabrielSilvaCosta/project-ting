47c164aad8ff5ad059a18219bd9423f0e0cce42b
GabrielSilvaCosta <bob@>
2023-10-28 15:45:11 -0300

    corrigido as função e feito.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,16 +3,10 @@ from ting_file_management.file_management import txt_importer
 
 
 def is_already_processed(queue_instance, file_path):
-    if hasattr(queue_instance, "items") and isinstance(
-        queue_instance.items, list
-    ):
-        for item in queue_instance.items:
-            if (
-                isinstance(item, dict)
-                and item.get("nome_do_arquivo") == file_path
-            ):
-                return True
-    return False
+    return any(
+        isinstance(item, dict) and item.get("nome_do_arquivo") == file_path
+        for item in getattr(queue_instance, "items", [])
+    )
 
 
 def process(path_file, queue_instance):
@@ -22,14 +16,13 @@ def process(path_file, queue_instance):
             return
 
         file_content = txt_importer(path_file)
-        queue_instance.enqueue(file_content)
-
-        file_metadata = {
+        metadata = {
             "nome_do_arquivo": path_file,
             "qtd_linhas": len(file_content),
             "linhas_do_arquivo": file_content,
         }
-        print(file_metadata)
+        queue_instance.enqueue(metadata)
+        print(metadata)
 
     except Exception as e:
         print(f"Erro ao processar {path_file}: {str(e)}")
@@ -37,14 +30,8 @@ def process(path_file, queue_instance):
 
 def remove(queue_instance):
     try:
-        if len(queue_instance) == 0:
-            print("Não há elementos")
-            return
-
-        removed_file = queue_instance.dequeue()
-        print(
-            f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso"
-        )
+        removed = queue_instance.dequeue()
+        print(f"Arquivo {removed.get('nome_do_arquivo')} removido com sucesso")
 
     except IndexError:
         print("Não há elementos")
@@ -56,13 +43,14 @@ def remove(queue_instance):
 def file_metadata(queue_instance, position):
     try:
         file_info = queue_instance.search(position)
-
         print(
             {
-                "nome_do_arquivo": file_info["nome_do_arquivo"],
-                "qtd_linhas": file_info["qtd_linhas"],
-                "linhas_do_arquivo": file_info["linhas_do_arquivo"],
+                "nome_do_arquivo": file_info.get("nome_do_arquivo"),
+                "qtd_linhas": file_info.get("qtd_linhas"),
+                "linhas_do_arquivo": file_info.get("linhas_do_arquivo"),
             }
+        ) if isinstance(file_info, dict) else print(
+            "Posição inválida", file=sys.stderr
         )
 
     except IndexError:
