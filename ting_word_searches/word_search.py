b7457917d7f5ab9245a6420a2079c9ad06ae5552
GabrielSilvaCosta <bob@>
2023-10-31 15:55:59 -0300

    requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return [
+        {
+            "palavra": word,
+            "arquivo": file_info["nome_do_arquivo"],
+            "ocorrencias": [
+                {
+                    "linha": line_number,
+                    "conteudo": line_content.strip(),
+                }
+                for line_number, line_content in enumerate(
+                    file_info["linhas_do_arquivo"], 1
+                )
+                if word.lower() in line_content.lower()
+            ],
+        }
+        for file_info in instance.items
+        if any(
+            word.lower() in line.lower()
+            for line in file_info["linhas_do_arquivo"]
+        )
+    ]
